783c4ceacf142f903bdc1988a2372755281ec5b2
m.khutornaya <m.khutornaya@vk.team>
2022-12-06 18:54:43 +0300

    start new project

---
+++ TheWeatherOutside/TheWeatherOutside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWeatherOutside
+//
+//  Created by m.khutornaya on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
