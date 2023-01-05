75c7fc2ab3e80e2de036f36d44a8b26a862c4d46
Maria Khutornaia <masha.hutornaya@gmail.com>
2023-01-05 19:17:13 +0100

    add uiPageController

---
+++ TheWeatherOutside/TheWeatherOutside/MainViewController/EmptyView.swift
@@ -0,0 +1,70 @@
+//
+//  EmptyView.swift
+//  WeatherWise
+//
+//  Created by Mariya Khutornaya on 05.01.23.
+//
+
+import UIKit
+
+final class EmptyView: UIView {
+    
+    private lazy var image: UIImageView = {
+        let view = UIImageView()
+
+        view.image = UIImage(named: "umbrella")
+
+        view.contentMode = .scaleToFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+    
+    private lazy var locationDescription: UILabel = {
+        let view = UILabel()
+
+        view.text = "EMPTY_LOCATION_DESCRIPTION".localized
+        view.textColor = .label
+        view.font = .rubikRegular(size: 16)
+        view.numberOfLines = 0
+        view.textAlignment = .center
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        backgroundColor = .background
+        let subviews = [image, locationDescription]
+        subviews.forEach { addSubview($0) }
+
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: 180),
+            image.heightAnchor.constraint(equalToConstant: 196),
+            image.centerYAnchor.constraint(equalTo: centerYAnchor),
+            image.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            locationDescription.topAnchor.constraint(equalTo: image.bottomAnchor, constant: .verticalMargin),
+            locationDescription.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .textSafeArea),
+            locationDescription.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.textSafeArea)
+        ])
+    }
+}
+
+private extension CGFloat {
+    static let imageHigh: CGFloat = 196
+    static let imageWidth: CGFloat = 180
+    
+    static let verticalMargin: CGFloat = 55
+
+    static let textSafeArea: CGFloat = 25
+}
--- TheWeatherOutside/TheWeatherOutside/ViewController.swift
+++ TheWeatherOutside/TheWeatherOutside/ViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    //    private let pageController: UIPageViewController = {
+    //        let pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    ////        pageController.dataSource = self
+    //        pageController.delegate = self
+    //        pageController.view.backgroundColor = .clear
+    //        pageController.didMove(toParent: self)
+    //
+    //        return pageController
+    //    }()
+    
+    private var pageController: UIPageViewController?
+    private var currentIndex: Int = 0
+    
+    private func setupPageController() {
+        pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageController?.dataSource = self
+        self.pageController?.delegate = self
+        self.pageController?.view.backgroundColor = .clear
+        self.pageController?.view.frame = CGRect(x: 0,y: 0,width: self.view.frame.width,height: self.view.frame.height)
+        
+        addChild(pageController!)
+        view.addSubview(pageController!.view)
+        self.pageController?.setViewControllers([EmptyViewController()], direction: .forward, animated: true, completion: nil)
+        self.pageController?.didMove(toParent: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .background
         setUpNavigationBar()
-//        ForecastApiManager().forecastRequest(lat: 50.119469, lon: 8.599217, units: .metric)
-//        ForecastApiManager().forecastRequest(lat: 43.271008, lon: 5.372398, units: .metric)
-//        ForecastApiManager().forecastRequest(lat: 55.755864, lon: 37.617698, units: .metric)
+        setupPageController()
+        //        ForecastApiManager().forecastRequest(lat: 50.119469, lon: 8.599217, units: .metric)
+        //        ForecastApiManager().forecastRequest(lat: 43.271008, lon: 5.372398, units: .metric)
+        //        ForecastApiManager().forecastRequest(lat: 55.755864, lon: 37.617698, units: .metric)
         //        GeoCodeApiManager().geoCodeRequest(for: "Москва")43.271008, 5.372398
     }
     
     private func setUpNavigationBar() {
         let navBar: UINavigationBar? = navigationController?.navigationBar
         navBar?.isHidden = false
-        navBar?.backgroundColor = .white
-        navBar?.tintColor = .black
+        navBar?.backgroundColor = .background
+        navBar?.tintColor = .icon
         
         let menuButtonItem = UIBarButtonItem(
             image: UIImage(named: "burgerMenu"),
@@ -50,14 +77,13 @@ final class ViewController: UIViewController {
     
     @objc
     func addLocation() {
-        let alertController = UIAlertController(title: "Загрузка фотографии", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(title: "ALERT_TITLE".localized, message: nil, preferredStyle: .alert)
         alertController.addTextField { textField in
-            textField.placeholder = "Введите название"
+            textField.placeholder = "ALERT_PLACEHOLDER".localized
         }
         
-        let saveImageAction = UIAlertAction(title: "Сохранить", style: .default) { [weak self] action in
-            guard let self = self,
-                  let text = alertController.textFields?[0].text,
+        let saveImageAction = UIAlertAction(title: "DONE".localized, style: .default) { action in
+            guard let text = alertController.textFields?[0].text,
                   text != ""
             else { return }
             
@@ -67,17 +93,40 @@ final class ViewController: UIViewController {
                       let lon = coordinates.longitude(),
                       let title = coordinates.name()
                 else { return }
-//                проверить есть ли такая локация уже
-//                если нет запрос
+                //                проверить есть ли такая локация уже
+                //                если нет запрос
                 // если есть -> открывать страницу с этой локацией
                 ForecastRepository().fetchDataForLocation(lat: lat, lon: lon, title: title)
             }
         }
         
-        let cancel = UIAlertAction(title: "Отменить", style: .cancel)
+        let cancel = UIAlertAction(title: "CANCEL".localized, style: .cancel)
         
         alertController.addAction(cancel)
         alertController.addAction(saveImageAction)
         present(alertController, animated: true)
+    }
+}
+
+extension ViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return UIViewController()
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return UIViewController()
+    }
+}
+
+extension ViewController: UIPageViewControllerDataSource {
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return 4
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return currentIndex
     }
 }
